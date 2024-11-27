f116035a91eb489150d7c71ba2143aa0e68e7559
fahedbelkheiri <fahed.belkheiri@gmail.com>
2024-11-27 22:42:33 +0100

    prime01

---
+++ prime-num.py
@@ -0,0 +1,31 @@
+import math
+
+
+def is_prime(N):
+    if N <= 1:
+        return False  
+    if N <= 3:
+        return True   
+    if N % 2 == 0 or N % 3 == 0:
+        return False  
+
+    
+    i = 5
+    while i * i <= N:
+        if N % i == 0 or N % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+def main():
+    
+    number = int(input("Enter an integer: "))
+
+    if is_prime(number):
+        print(f"{number} is a prime number.")
+    else:
+        print(f"{number} is not a prime number.")
+
+if __name__== "__main__":
+    main()
